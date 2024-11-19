0c5996f34e945bb87dcfbbec3787fbf95e21aedc
juliacora <juliacora@gmail.com>
2024-11-19 14:18:56 -0300

    Verificao de arquivo existente

--- AT01-EXEMPLO/main.js
+++ AT01-EXEMPLO/main.js
@@ -17,14 +17,29 @@ window.addEventListener("load", ()=>{
         request.onreadystatechange = function(){
             const textoCaixa = document.querySelector("#caixa")
 
-            textoCaixa.innerHTML = this.responseText;
+            // this == pedido
+            // verifica se o arquivo foi encontrado e o status é OK
+            if(this.readyState == 4 && this.status == 200){
+                textoCaixa.innerHTML = this.responseText;
+            } else {
+                textoCaixa.innerHTML = 'ERRO TENTE MAIS TARDE'
+            }
+
+            
         }
 
-        request.open("GET", "./texto.txt", true)
+        const urlCerta =  "./texto.txt"
+        const urlErrada =  "./abcde.txt"
+
+        // experimnetar usar a url certa
+        request.open("GET", urlCerta, true)
+
+        // USAR POST CASO O ARQUIVO NAO SERÀ DEVOLVIDO (EX: CRIAR PASTAS NO SERVIDOR, ATUALIZAR BASE DE DADOS SERVIDOR)
+        // USAR POST QUANDO DESEJAMOS ENVIAR UMA QUANTIDADE MAIOR DE INFORMACOES (MAIS SEGURO, MAIS TAMANHO, MAIS ROBUSTO)
+        // USAR POST QUANDO PEGAR FORMULARIOS DE HTML
+        
 
         request.send();
-
-
 
 
     })
